40987dda9b4dd87682ba979eaac6239d5ada8e56
nazwisko.imie <209523@edu.p.lodz.pl>
2019-03-06 21:11:29 +0100

    pierwszy commit

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -5,8 +5,18 @@ import java.util.List;
 
 public class TailsGenerator {
 
-    public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
-    }
+	public List<String> tails(String value) {
+		if (value == null) {
+			return new ArrayList<>(0);
+		}
 
+		ArrayList<String> tails = new ArrayList<String>();
+
+		for (int i = 0; i < value.length(); i++) {
+			tails.add(value.substring(i));
+		}
+
+		tails.add("");
+		return tails;
+	}
 }
